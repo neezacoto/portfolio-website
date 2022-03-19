16903fb9772c15aee2ffeb21ea0be2b3f353e293
Christian Rudder <ringthebellpepper@gmail.com>
2022-03-19 18:53:13 -0400

    mobile view is fully responsive with show more button that will scroll back when closed

--- website/src/components/LandingPage/PersonalSection.js
+++ website/src/components/LandingPage/PersonalSection.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { BiChevronUp } from 'react-icons/bi'
+import { Link } from 'react-scroll'
 
 function PersonalSection(props) {
+
+    const [toggleRead,setToggleRead] = useState(false)
+    let readText = (!toggleRead)? 'show more' : 'show less';
+    const showPersonal = () => {
+        setToggleRead(!toggleRead);
+    }
+
+
   return (
     <section id="personal-section">
             <div className='personal-content-container'>
@@ -25,7 +35,7 @@ function PersonalSection(props) {
                         </div>
                     </div>
                     <div className='bio' style={props.parallax.personalBio}>
-                            <p>
+                            {/* <p>
                                 Other than spending my free time on coding, you'll find me involved in the arts and taking in nature.<br/><br/>
                                 When it comes to art, my medium of choice is digital, with a mix of cartoon/semi-realism. I've been designing
                                 and doing art for as long as I can remember. It comes in handy when working on coding projects, because
@@ -33,9 +43,42 @@ function PersonalSection(props) {
                                 but if the design isn't there, it all falls apart.<br/><br/> My favorite part about the outdoors are the clouds. It feels
                                 absolutely surreal to have these giants in the sky, looking as if they painted. Nature as a whole I believe is something beautiful, 
                                 becaues no one moment is ever the same—ever fleeting, ever beautiful.
+                            </p> */}
+                            <div className='read-container' data-visible={toggleRead} id='read-bio'>
+                                <div className='cover' />
+                                <p>
+                                    Other than spending my free time on coding, you'll find me involved in the arts and taking in nature.<br/><br/>
+                                    When it comes to art, my medium of choice is digital, with a mix of cartoon/semi-realism. I've been designing
+                                    and doing art for as long as I can remember. It comes in handy when working on coding projects, because
+                                    I'm always laser focused on functionality and presentation. I believe that someone can create an amazing tool, 
+                                    but if the design isn't there, it all falls apart.<br/><br/> My favorite part about the outdoors are the clouds. It feels
+                                    absolutely surreal to have these giants in the sky, looking as if they painted. Nature as a whole I believe is something beautiful, 
+                                    becaues no one moment is ever the same—ever fleeting, ever beautiful.
+                                </p>
+                            </div>
+                            {(!toggleRead)? 
+                                <button data-visible={toggleRead} className='read-more' for='read' onClick={showPersonal}>
+                                    {readText}
+                                    <BiChevronUp className='pointer'/>
+                                </button> 
+                                :
+                                <Link
+                                    activeClass="active"
+                                    to="read-bio"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-300}
+                                    duration={500}
+                                >
+                                    <button data-visible={toggleRead} className='read-more' for='read' onClick={showPersonal}>
+                                        {readText}
+                                        <BiChevronUp className='pointer'/>
+                                    </button> 
+                                 </Link>
 
-                                
-                            </p>
+                            }
+                            
+                             
                     </div>
                 </div>
             </div>
