c58fce9aeb15f7e287415919d6512bd9c2b925cb
Christian Rudder <ringthebellpepper@gmail.com>
2022-03-20 18:05:01 -0400

    footer wording

--- website/src/components/LandingPage/Footer.js
+++ website/src/components/LandingPage/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer-container'>
         <p>©2022 Christian Rudder</p>
-        <a href="https://github.com/neezacoto/portfolio-website" >view source code for this website</a>   
+        <a href="https://github.com/neezacoto/portfolio-website" >view source code</a>   
     </div>
   )
 }
